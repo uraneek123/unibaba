dd3783b8efc550b5ae0f16aa7e85904736611936
TheRealNeek <raymond.liu6@education.nsw.gov.au>
2025-03-14 21:27:22 +1100

    first

---
+++ base/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+from .forms import LoginForm
+
+app_name = 'base'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'), #this is where all page urls are implemented, can reference in href
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='base/login.html',authentication_form=LoginForm), name='login'),
+]
---
+++ base/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+
+from item.models import Category, Item
+
+from .forms import SignupForm
+
+# Create your views here.
+
+def index(request):
+    items = Item.objects.filter(is_sold=False)[0:6] #get 6 items that are not sold to display on front page
+    categories = Category.objects.all()
+    return render(request,'base/index.html', {
+        'categories': categories,
+        'items': items,
+    })
+
+def contact(request):
+    return render(request, 'base/contact.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form=SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+    
+    form = SignupForm()
+    return render(request, 'base/signup.html', {
+        'form':form
+    })
