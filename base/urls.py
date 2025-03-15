f5313ed1d25dad3df933cd5d88cdd55ef68926c0
Judore <76834322+Judore@users.noreply.github.com>
2025-03-15 11:58:33 +1100

    changed buttons and logo

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 from .forms import LoginForm
 
+
 app_name = 'base'
 
 urlpatterns = [
@@ -10,4 +11,4 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'), #this is where all page urls are implemented, can reference in href
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='base/login.html',authentication_form=LoginForm), name='login'),
-]
+] 
